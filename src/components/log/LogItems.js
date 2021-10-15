fdd7db855540830d25c603ac071153d1a3d45f39
Muhammad Ibrahim <ibrahimraju347@gmail.com>
2021-10-15 15:28:32 +0500

    set time date and name of developer
    Purpose which developer to edit the page

---
+++ src/components/log/LogItems.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Moment from 'react-moment';
+
+const LogItems = ({ log }) => {
+  return (
+    <li className="collection-item">
+      <div>
+        {/* edit log modal is id to open new page and modal-trigger to use to open the modal like edit log modal */}
+        <a
+          href="#edit-log-modal"
+          className={`modal-trigger ${
+            log.attention ? 'red-text' : 'blue-text'
+          }`}
+        >
+          {log.message}
+        </a>
+        <br />
+        <span>
+          ID#{log.id}last update by {log.developer}
+          <Moment format=" MMMM Do YYYY, h:mm:ss a">{log.date}</Moment>
+        </span>
+      </div>
+    </li>
+  );
+};
+
+LogItems.propTypes = {
+  log: PropTypes.object.isRequired,
+};
+
+export default LogItems;
