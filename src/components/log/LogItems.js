2baa1916fa4f3ac2c85393db5dd5493399d04068
Muhammad Ibrahim <ibrahimraju347@gmail.com>
2021-10-17 23:37:16 +0500

    update server

--- src/components/log/LogItems.js
+++ src/components/log/LogItems.js
@@ -20,7 +20,7 @@ const LogItems = ({ log }) => {
           ID#{log.id}last update by {log.developer}
           <Moment format=" MMMM Do YYYY, h:mm:ss a">{log.date}</Moment>
         </span>
-        secondary-content to left side pa lya jayta ha
+        {/* secondary-content to left side pa lya jayta ha */}
         <a href="#" className="secondary-content">
           <i className="material-icons grey-text">delete</i>
         </a>
