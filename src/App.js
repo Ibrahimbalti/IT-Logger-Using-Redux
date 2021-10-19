c12ba8fbbb3f9239ec5d8aa3a0f82901084df8a8
Muhammad Ibrahim <ibrahimraju347@gmail.com>
2021-10-19 12:12:40 +0500

    complete add developer modal

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import SearchBar from './components/layout/SearchBar';
 import AddLogModal from './components/log/AddLogModal';
 import EditLogModal from './components/log/EditLogModal';
+import AddDeveloperModal from './components/developers/AddDeveloperModal';
 import Logs from './components/log/Logs';
 import AddBtn from './components/layout/AddBtn';
 import 'materialize-css/dist/css/materialize.min.css';
@@ -21,6 +22,7 @@ function App() {
         <AddBtn />
         <AddLogModal />
         <EditLogModal />
+        <AddDeveloperModal />
         <Logs />
       </div>
     </Fragment>
---
+++ src/components/developers/AddDeveloperModal.js
@@ -0,0 +1,62 @@
+import React, { useState } from 'react';
+import M from 'materialize-css/dist/js/materialize.min.js';
+
+const AddLogModal = () => {
+  const [firstname, setFirstname] = useState('');
+  const [lastname, setLastname] = useState('');
+
+  const onSubmit = () => {
+    if (firstname === '' || lastname === '') {
+      M.toast({ html: 'Please fill the first and last name fields' });
+    } else {
+      console.log(firstname, lastname);
+      setFirstname();
+      setLastname();
+    }
+  };
+  return (
+    <div id="developer-modal" className="modal">
+      <div className="modal-content">
+        <h4>Enter Developer </h4>
+        <div className="row">
+          <div className="input-field">
+            <input
+              type="text"
+              name="firstname"
+              value={firstname}
+              onChange={(e) => setFirstname(e.target.value)}
+            />
+            <label htmlFor="Firstname" className="active">
+              First Name
+            </label>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="input-field">
+            <input
+              type="text"
+              name="lastname"
+              value={lastname}
+              onChange={(e) => setLastname(e.target.value)}
+            />
+            <label htmlFor="lastname" className="active">
+              Last Name
+            </label>
+          </div>
+        </div>
+      </div>
+      <div className="modal-footer">
+        <a
+          href="#!"
+          onClick={onSubmit}
+          className="modal-close waves-effect  waves-blue btn-flat"
+        >
+          Submit
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default AddLogModal;
